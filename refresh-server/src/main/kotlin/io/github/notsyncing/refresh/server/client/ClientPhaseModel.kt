1d51d315193d9a12a908668e9a6a2661d7870316
notsyncing <song.fc@gmail.com>
2017-02-26 20:56:52 +0800

    server: 更新依赖库版本。

--- refresh-server/src/main/kotlin/io/github/notsyncing/refresh/server/client/ClientPhaseModel.kt
+++ refresh-server/src/main/kotlin/io/github/notsyncing/refresh/server/client/ClientPhaseModel.kt
@@ -3,7 +3,7 @@ package io.github.notsyncing.refresh.server.client
 import io.github.notsyncing.lightfur.entity.EntityModel
 
 class ClientPhaseModel : EntityModel(table = "client_phases") {
-    var accountId: String by field(this::accountId, column = "account_id", primaryKey = true)
+    var accountId: String by field(column = "account_id", primaryKey = true)
 
-    var phase: Int by field(this::phase, column = "phase")
+    var phase: Int by field(column = "phase")
 }

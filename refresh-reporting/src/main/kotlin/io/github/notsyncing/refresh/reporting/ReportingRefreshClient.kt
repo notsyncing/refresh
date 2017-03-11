e751f9bf6b0308e2a364dd97ec08f08a3824f53f
notsyncing <song.fc@gmail.com>
2017-03-11 21:28:24 +0800

    reporting: 成功上报后记录时间。

--- refresh-reporting/src/main/kotlin/io/github/notsyncing/refresh/reporting/ReportingRefreshClient.kt
+++ refresh-reporting/src/main/kotlin/io/github/notsyncing/refresh/reporting/ReportingRefreshClient.kt
@@ -7,9 +7,15 @@ import io.github.notsyncing.refresh.app.utils.asStringAsyncFuture
 import io.github.notsyncing.refresh.common.enums.OperationResult
 import kotlinx.coroutines.experimental.future.await
 import kotlinx.coroutines.experimental.future.future
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ReportingRefreshClient(refresher: Refresher) : RefreshSubClient(refresher) {
+    private val timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
     fun reportFile(file: Path) = future<OperationResult> {
         val fn = file.fileName.toString()
 
@@ -20,9 +26,26 @@ class ReportingRefreshClient(refresher: Refresher) : RefreshSubClient(refresher)
                 .await()
 
         if (r == OperationResult.Success.ordinal.toString()) {
+            val lastReportedData = timeFormatter.format(LocalDateTime.now())
+            val lastReportedFlag = refresher.appDir.resolve(".last_reported")
+
+            Files.write(lastReportedFlag, lastReportedData.toByteArray(), StandardOpenOption.CREATE)
+
             return@future OperationResult.Success
         } else {
             return@future OperationResult.Failed
         }
+    }
+
+    fun getLastReportedTime(): LocalDateTime? {
+        val lastReportedFlag = refresher.appDir.resolve(".last_reported")
+
+        if (!Files.exists(lastReportedFlag)) {
+            return null
+        }
+
+        val lastReportedData = String(Files.readAllBytes(lastReportedFlag))
+
+        return LocalDateTime.parse(lastReportedData, timeFormatter)
     }
 }

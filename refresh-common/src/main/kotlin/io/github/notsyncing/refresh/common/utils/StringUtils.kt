2a03ab0aacda41e9d23ff14d852a11795093805f
notsyncing <song.fc@gmail.com>
2017-03-17 14:17:35 +0800

    common: 修复了从字节数组生成十六进制字符串错误的问题。

--- refresh-common/src/main/kotlin/io/github/notsyncing/refresh/common/utils/StringUtils.kt
+++ refresh-common/src/main/kotlin/io/github/notsyncing/refresh/common/utils/StringUtils.kt
@@ -2,7 +2,6 @@ package io.github.notsyncing.refresh.common.utils
 
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
-import kotlin.experimental.and
 
 fun String?.sha256(): String? {
     var strResult: String? = null
@@ -42,12 +41,18 @@ fun String?.sha256Salted(): String? {
     return stage2
 }
 
-fun ByteArray.toHexString(): String {
-    var result = ""
+private val HEX_CHARS = "0123456789abcdef".toCharArray()
 
-    for (i in this.indices) {
-        result += Integer.toString((this[i] and 0xff.toByte()) + 0x100, 16).substring(1)
+fun ByteArray.toHexString(): String {
+    val result = StringBuffer()
+
+    forEach {
+        val octet = it.toInt()
+        val firstIndex = (octet and 0xF0).ushr(4)
+        val secondIndex = octet and 0x0F
+        result.append(HEX_CHARS[firstIndex])
+        result.append(HEX_CHARS[secondIndex])
     }
 
-    return result
+    return result.toString()
 }
